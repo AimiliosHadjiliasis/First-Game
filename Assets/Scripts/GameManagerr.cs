b6d5df9f747747ba5df6fb180e75485c81ba629e
Aimilios <aimilioshadjiliasis@gmail.com>
2020-06-20 17:39:30 +0300

    Final Upload for this mini project: 1) Create an end model and use it as a trigger to see if we have reach the end 2) Create a new panel with some text and use them to create an animation that is appearing when we finish a level 3) Create multiple levels 4) Create starting menu 5) Create end Menu (Credits)

--- Assets/Scripts/GameManagerr.cs
+++ Assets/Scripts/GameManagerr.cs
@@ -7,6 +7,16 @@ public class GameManagerr : MonoBehaviour
 {
     bool gameHasEnded = false;
     public float restartDelay = 1f;
+
+    //we create a game object that we enable it when we reach the end
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    // function that identifies that the game has ended
     public void EndGame()
     {
         if (gameHasEnded == false)
@@ -14,10 +24,11 @@ public class GameManagerr : MonoBehaviour
             gameHasEnded = true;
             Debug.Log("Game Over");
             //Restart the game:
-            Invoke("Restart", restartDelay);
+            Invoke("Restart", restartDelay); // adds a delay 
         }
     }
 
+    //Function that restarts the game
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
